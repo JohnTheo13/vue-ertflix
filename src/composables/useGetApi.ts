67f484f64c8b4a1b0ce5044fe3b02e0a043814e2
Ioannis Theodosiou <theodosioujohn88@gmail.com>
2025-11-26 21:19:30 +0100

    basic get request functionalities

---
+++ src/composables/useGetApi.ts
@@ -0,0 +1,28 @@
+import { ref } from 'vue';
+import { apiFetch, type ParsedError } from '~/api';
+
+export function useGetApi<T>(
+  url: string,
+  enabled: boolean = true,
+  options: RequestInit = {},
+) {
+  const data = ref<T | null>(null);
+  const error = ref<ParsedError | null>(null);
+  const loading = ref<boolean>(true);
+  const fetchData = async () => {
+    try {
+      const response = await apiFetch(url, options);
+      data.value = response;
+    } catch (err: unknown) {
+      error.value = err as ParsedError;
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  if (enabled) {
+    fetchData();
+  }
+
+  return { data, error, loading, fetchData };
+}
